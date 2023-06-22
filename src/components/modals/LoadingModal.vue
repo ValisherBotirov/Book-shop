a80626412fa55c672bb2e1a87aa4864ea265b51d
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-06-22 21:05:54 +0500

    add

--- src/components/modals/LoadingModal.vue
+++ src/components/modals/LoadingModal.vue
@@ -3,6 +3,8 @@
     <div class="fixed z-[999999] inset-0 bg-[rgba(0,0,0,0.4)] backdrop-blur-sm">
       <div class="fixed z-[999999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <img :src="LoadingSvg" alt="loading svg" />
+<!--        <img src="https://i.gifer.com/7YRI.gif" alt="loading svg" />-->
+
       </div>
     </div>
   </Teleport>
