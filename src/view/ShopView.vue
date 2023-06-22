bc551690954b311a7244301dd5fbcfb3048dc12c
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-06-22 17:38:10 +0500

    fix: search comp

--- src/view/ShopView.vue
+++ src/view/ShopView.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="space-y-6">
-    <SearchFormCompVue />
-    <ShortNavCompVue />
+    <ShortNavCompVue class="mt-3" />
     <SwiperBigCompVue />
     <SwiperCartCompVue />
   </div>
 </template>
 
 <script setup>
-import SearchFormCompVue from "../components/shop/SearchFormComp.vue";
 import ShortNavCompVue from "../components/shop/ShortNavComp.vue";
 import SwiperBigCompVue from "../components/shop/SwiperBigComp.vue";
 import SwiperCartCompVue from "../components/shop/SwiperCartComp.vue";
