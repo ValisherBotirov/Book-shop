b2f1ce0c3162ec8fbc9591b70510f27c36795df6
Ikromov-fs <nodirikromov10@gmail.com>
2023-06-24 18:10:48 +0500

    styles

--- src/components/modals/LoginModal.vue
+++ src/components/modals/LoginModal.vue
@@ -1,10 +1,19 @@
 <template>
   <Teleport to="body">
-    <div @click="emit('closeLoginModal')" class="fixed z-[999999] inset-0 bg-[#0006] backdrop-blur-sm"></div>
-    <div class="fixed z-[999999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div
+      @click="emit('closeLoginModal')"
+      class="fixed z-[999999] inset-0 bg-[#0006] backdrop-blur-sm"
+    ></div>
+    <div
+      class="fixed z-[999999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+    >
       <div class="loginModalbg p-16 text-[#F4F6F9] text-lg relative">
         <div class="flex flex-col gap-6">
-          <img class="w-28 self-center mb-8" src="../../assets/login/logo.png" alt="" />
+          <img
+            class="w-28 self-center mb-8"
+            src="../../assets/login/logo.png"
+            alt=""
+          />
           <form class="flex flex-col gap-6">
             <div class="flex flex-col">
               <div class="mb-4 flex flex-col">
@@ -16,9 +25,11 @@
                   id="email"
                   placeholder="john@gmail.com"
                 />
-                <span class="text-sm text-end text-red-600" v-if="v$.email.$error">{{
-                  v$.email.$errors[0].$message
-                }}</span>
+                <span
+                  class="text-sm text-end text-red-600"
+                  v-if="v$.email.$error"
+                  >{{ v$.email.$errors[0].$message }}</span
+                >
               </div>
               <div class="flex flex-col">
                 <input
@@ -29,9 +40,11 @@
                   id="password"
                   placeholder="Пароль"
                 />
-                <span class="text-sm text-end text-red-600" v-if="v$.password.$error">{{
-                  v$.password.$errors[0].$message
-                }}</span>
+                <span
+                  class="text-sm text-end text-red-600"
+                  v-if="v$.password.$error"
+                  >{{ v$.password.$errors[0].$message }}</span
+                >
               </div>
             </div>
             <div class="flex justify-center">
@@ -43,15 +56,24 @@
             </div>
           </form>
           <div class="flex justify-between items-center">
-            <div @click="emit('changeTo')" class="underline hover:no-underline active:no-underline cursor-pointer">
+            <div
+              @click="emit('changeTo')"
+              class="underline hover:no-underline active:no-underline cursor-pointer"
+            >
               {{ $t("register") }}
             </div>
-            <div @click="emit('changeTo')" class="underline hover:no-underline active:no-underline cursor-pointer">
+            <div
+              @click="emit('changeTo')"
+              class="underline hover:no-underline active:no-underline cursor-pointer"
+            >
               Забыли пароль?
             </div>
           </div>
         </div>
-        <div @click="emit('closeLoginModal')" class="absolute top-3 right-3 text-xl">
+        <div
+          @click="emit('closeLoginModal')"
+          class="absolute top-3 right-3 text-xl"
+        >
           <i class="fa-solid fa-xmark p-3 duration-200 hover:opacity-40"></i>
         </div>
       </div>
@@ -61,7 +83,13 @@
 
 <script setup>
 import { ref, reactive, computed } from "vue";
-import { required, email, minLength, helpers, maxLength } from "@vuelidate/validators";
+import {
+  required,
+  email,
+  minLength,
+  helpers,
+  maxLength,
+} from "@vuelidate/validators";
 import { useVuelidate } from "@vuelidate/core";
 
 import ButtonFillVue from "../buttons/ButtonFill.vue";
@@ -81,18 +109,30 @@ const rules = computed(() => {
       required,
       minLength: minLength(8),
       maxLength: maxLength(32),
-      containsUppercase: helpers.withMessage("The password requires an uppercase character", function (value) {
-        return /[A-Z]/.test(value);
-      }),
-      containsLowercase: helpers.withMessage("The password requires an lowercase character", function (value) {
-        return /[a-z]/.test(value);
-      }),
-      containsNumber: helpers.withMessage("The password requires an number character", function (value) {
-        return /[0-9]/.test(value);
-      }),
-      containsSpecial: helpers.withMessage("The password requires an special character", function (value) {
-        return /[#?!_@$%^&*.-]/.test(value);
-      }),
+      containsUppercase: helpers.withMessage(
+        "The password requires an uppercase character",
+        function (value) {
+          return /[A-Z]/.test(value);
+        }
+      ),
+      containsLowercase: helpers.withMessage(
+        "The password requires an lowercase character",
+        function (value) {
+          return /[a-z]/.test(value);
+        }
+      ),
+      containsNumber: helpers.withMessage(
+        "The password requires an number character",
+        function (value) {
+          return /[0-9]/.test(value);
+        }
+      ),
+      containsSpecial: helpers.withMessage(
+        "The password requires an special character",
+        function (value) {
+          return /[#?!_@$%^&*.-]/.test(value);
+        }
+      ),
     },
   };
 });
@@ -117,7 +157,11 @@ const emit = defineEmits(["closeLoginModal", "changeTo"]);
 
 <style>
 .loginModalbg {
-  background: linear-gradient(180deg, rgba(79, 135, 211, 0.95) 0%, rgba(32, 95, 184, 0.95) 100%);
+  background: linear-gradient(
+    180deg,
+    rgba(79, 135, 211, 0.95) 0%,
+    rgba(32, 95, 184, 0.95) 100%
+  );
   border: 3px solid #f4f6f9;
   border-radius: 33px;
 }
--- src/components/modals/RegisterModal.vue
+++ src/components/modals/RegisterModal.vue
@@ -1,72 +1,98 @@
 <template>
   <Teleport to="body">
-    <div @click="emit('closeRegiterModal')" class="fixed z-[999999] bg-[#0006] inset-0 backdrop-blur-sm"></div>
-    <div class="fixed z-[999999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-lg">
-      <div class="loginModalbg px-16 py-4 text-[#F4F6F9] text-lg relative">
+    <div
+      @click="emit('closeRegiterModal')"
+      class="fixed z-[999999] bg-[#0006] inset-0 backdrop-blur-sm"
+    ></div>
+    <div
+      class="fixed z-[999999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-lg sm:w-[50%]"
+    >
+      <div class="px-16 py-5 text-lg relative bg-[#fafcf5] rounded-lg">
         <div class="flex flex-col gap-6">
-          <img class="w-28 self-center mb-1" src="../../assets/login/logo.png" alt="logo_img" />
-          <form class="flex flex-col gap-2 mb-8">
+          <form class="flex flex-col gap-2 mb-8 text-black">
             <div class="name flex flex-col gap-0">
               <label for="username text-sm">Логин:</label>
               <input
-                class="bg-transparent border min-w-[16rem] border-white rounded-xl py-1 px-3 text-[#f4f4f9] outline-none text-center placeholder:text-[#f4f6f927]"
+                class="border border-solid border-black min-w-[16rem] rounded-[6px] py-1 px-3 text-[#333] outline-none text-start placeholder:text-[#000]"
                 type="text"
                 v-model="userData.username"
                 name="username"
                 id="username"
                 placeholder="Введите ваш логин"
               />
-              <p v-if="v$.username.$error" class="text-sm text-end text-red-600">
+              <p
+                v-if="v$.username.$error"
+                class="text-sm text-end text-red-600"
+              >
                 {{ v$.username.$errors[0].$message }}*
               </p>
             </div>
             <div class="email flex flex-col gap-0">
               <label for="email">Электронная почта:</label>
               <input
-                class="bg-transparent border min-w-[16rem] border-white rounded-xl py-1 px-3 text-[#f4f4f9] outline-none text-center placeholder:text-[#f4f6f927]"
+                class="border border-solid border-black min-w-[16rem] rounded-[6px] py-1 px-3 text-[#333] outline-none text-start placeholder:text-[#333]"
                 type="email"
                 v-model="userData.email"
                 name="email"
                 id="email"
                 placeholder="Введите электронной почты"
               />
-              <p v-if="v$.email.$error" class="text-sm text-end text-red-600">{{ v$.email.$errors[0].$message }}*</p>
+              <p v-if="v$.email.$error" class="text-sm text-end text-red-600">
+                {{ v$.email.$errors[0].$message }}*
+              </p>
             </div>
             <div class="password flex flex-col gap-0">
               <label for="password">Пароль:</label>
               <input
-                class="bg-transparent capitalize border min-w-[16rem] border-white rounded-xl py-1 px-3 text-[#f4f4f9] outline-none text-center placeholder:text-[#F4F6F9B2]"
+                class="capitalize border min-w-[16rem] border-black rounded-[6px] py-1 px-3 text-[#333] outline-none text-start placeholder:text-[#333]"
                 type="password"
                 v-model="userData.password"
                 name="password"
                 id="password"
               />
-              <p v-if="v$.password.$error" class="text-sm text-end text-red-600">
+              <span class="relative cursor-pointer"><i class="fa-solid fa-eye absolute right-1 bottom-2"></i></span>
+              <span class="relative cursor-pointer"><i class="fa-solid fa-eye-slash absolute right-1 bottom-2"></i></span>
+              <p
+                v-if="v$.password.$error"
+                class="text-sm text-end text-red-600"
+              >
                 {{ v$.password.$errors[0].$message }}*
               </p>
             </div>
             <div class="confirmPassword flex flex-col gap-0">
               <label for="passwordConfirm">Подтвердите пароль:</label>
               <input
-                class="bg-transparent capitalize border min-w-[16rem] border-white rounded-xl py-1 px-3 text-[#f4f4f9] outline-none text-center placeholder:text-[#F4F6F9B2]"
+                class="capitalize border min-w-[16rem] border-black rounded-[6px] py-1 px-3 text-[#333] outline-none text-start placeholder:text-[#000]"
                 type="password"
                 v-model="userData.passwordConfirm"
                 name="passwordConfirm"
                 id="passwordConfirm"
               />
-              <p v-if="v$.passwordConfirm.$error" class="text-sm text-end text-red-600">
+              <span class="relative cursor-pointer"><i class="fa-solid fa-eye absolute right-1 bottom-2"></i></span>
+              <span class="relative cursor-pointer"><i class="fa-solid fa-eye-slash absolute right-1 bottom-2"></i></span>
+              <p
+                v-if="v$.passwordConfirm.$error"
+                class="text-sm text-end text-red-600"
+              >
                 {{ v$.passwordConfirm.$errors[0].$message }}*
               </p>
             </div>
-
-            <div class="flex justify-center mt-3">
+            <div>
               <ButtonFillVue>
-                <button @click.prevent="handleRegister" class="py-1 px-4">Далее</button>
+                <button
+                  @click.prevent="handleRegister"
+                  class="py-1 px-4 w-full"
+                >
+                  Далее
+                </button>
               </ButtonFillVue>
             </div>
           </form>
         </div>
-        <div @click="emit('closeRegiterModal')" class="absolute top-3 right-3 text-xl">
+        <div
+          @click="emit('closeRegiterModal')"
+          class="absolute top-3 right-3 text-xl"
+        >
           <i class="fa-solid fa-xmark p-3 duration-200 hover:opacity-40"></i>
         </div>
       </div>
@@ -77,10 +103,18 @@
 <script setup>
 import { computed, reactive, ref } from "vue";
 import { useVuelidate } from "@vuelidate/core";
-import { required, email, sameAs, minLength, maxLength } from "@vuelidate/validators";
+import {
+  required,
+  email,
+  sameAs,
+  minLength,
+  maxLength,
+} from "@vuelidate/validators";
 
 import { useUserRegister } from "../../store/UserRegister";
 import ButtonFillVue from "../buttons/ButtonFill.vue";
+
+const eyeHidden = ref(false);
 
 const store = useUserRegister();
 
@@ -119,11 +153,3 @@ const handleRegister = async () => {
 
 const emit = defineEmits(["closeRegiterModal"]);
 </script>
-
-<style>
-.loginModalbg {
-  background: linear-gradient(180deg, rgba(79, 135, 211, 0.95) 0%, rgba(32, 95, 184, 0.95) 100%);
-  border: 3px solid #f4f6f9;
-  border-radius: 33px;
-}
-</style>
