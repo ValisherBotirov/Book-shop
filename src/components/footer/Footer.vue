55427c34b38d3a47093efc0c0ecab1cc6c786ca2
Ikromov-fs <nodirikromov10@gmail.com>
2023-06-24 13:03:26 +0500

    foter

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,93 +1,81 @@
 <template>
   <footer id="footer" class="absolute w-full bottom-0">
-    <div class="w-[95%] mx-auto">
+    <div class="w-[100%] mx-auto">
       <div
         class="px-4 bg-[#444444BF] drop-shadow-lg rounded-t-2xl py-3 gap-4 flex flex-wrap flex-col md:flex-row justify-between items-center shadow-lg text-white"
       >
+        <div>
+          <ul>
+            <li class="text-[20px]">Информация</li>
+            <!-- <li class="text-[20px]">Часто задаваемые вопросы</li>
+            <li class="text-[20px]">Пункты выдачи</li>
+            <li class="text-[20px]">Блог</li> -->
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Для связи</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Наши достижения</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Виды оплаты</li>
+          </ul>
+        </div>
         <div class="flex gap-3 items-center flex-wrap">
           <span>{{ t("footer.social") }}:</span>
           <div class="flex gap-2 flex-wrap">
-            <a target="_blank" class="inline-block" href="http://t.me/STAR_NEW_TECH">
-              <img :src="icon1" alt="icon" class="duration-200 hover:-translate-y-1 w-6 h-6" />
+            <a
+              target="_blank"
+              class="inline-block"
+              href="http://t.me/STAR_NEW_TECH"
+            >
+              <img
+                :src="icon1"
+                alt="icon"
+                class="duration-200 hover:-translate-y-1 w-6 h-6"
+              />
             </a>
-            <a target="_blank" href="https://www.facebook.com/profile.php?id=100088042748728&mibextid=ZbWKwL">
-              <img :src="icon2" alt="icon" class="duration-200 hover:-translate-y-1 w-6 h-6" />
+            <a
+              target="_blank"
+              href="https://www.facebook.com/profile.php?id=100088042748728&mibextid=ZbWKwL"
+            >
+              <img
+                :src="icon2"
+                alt="icon"
+                class="duration-200 hover:-translate-y-1 w-6 h-6"
+              />
             </a>
             <a target="_blank" href="https://www.youtube.com/@Star_New_Tech">
-              <img :src="icon3" alt="icon" class="duration-200 hover:-translate-y-1 w-6 h-6" />
+              <img
+                :src="icon3"
+                alt="icon"
+                class="duration-200 hover:-translate-y-1 w-6 h-6"
+              />
             </a>
-            <a target="_blank" href="https://instagram.com/starnewtech?igshid=ZDdkNTZiNTM">
-              <img :src="icon4" alt="icon" class="duration-200 hover:-translate-y-1 w-6 h-6" />
+            <a
+              target="_blank"
+              href="https://instagram.com/starnewtech?igshid=ZDdkNTZiNTM"
+            >
+              <img
+                :src="icon4"
+                alt="icon"
+                class="duration-200 hover:-translate-y-1 w-6 h-6"
+              />
             </a>
           </div>
         </div>
 
-        <div class="SearchForm hidden xs:block">
-          <form>
-            <div class="SearchForm flex items-center p-0 border border-[#F4F6F940] rounded-3xl overflow-hidden">
-              <input
-                type="text"
-                class="searchInp pl-3 py-[2px] outline-none bg-transparent"
-                placeholder="Ваш номер"
-                value="+998 (98) 1801808"
-                disabled
-              />
-              <input
-                type="text"
-                class="searchInp px-0 py-[2px] outline-none bg-transparent"
-                placeholder="Ваш номер"
-                value="+998 (71) 2250015"
-                disabled
-              />
-              <a
-                href="tel:+998981801808"
-                class="all px-6 py-[2px] rounded-l-2xl text-primary font-semibold cursor-pointer bg-whiteBlue"
-              >
-                {{ t("footer.call") }}
-              </a>
-            </div>
-          </form>
-        </div>
-        <!--          responsive qismi-->
+        <!-- <div class="SearchForm hidden xs:block"></div> -->
+        <!--responsive qismi-->
 
-        <div class="SearchForm block xs:hidden">
-          <form>
-            <div class="SearchForm flex items-center p-0 border border-[#F4F6F940] rounded-3xl overflow-hidden">
-              <input
-                type="text"
-                class="searchInp pl-3 py-[2px] outline-none bg-transparent"
-                placeholder="Ваш номер"
-                value="+998 (98) 1801808"
-                disabled
-              />
-              <a
-                href="tel:+998981801808"
-                class="all px-6 py-[2px] rounded-l-2xl text-primary font-semibold cursor-pointer bg-whiteBlue"
-              >
-                {{ t("footer.call") }}
-              </a>
-            </div>
-          </form>
-          <form class="mt-4">
-            <div class="SearchForm flex items-center p-0 border border-[#F4F6F940] rounded-3xl overflow-hidden">
-              <input
-                type="text"
-                class="searchInp pl-3 py-[2px] outline-none bg-transparent"
-                placeholder="Ваш номер"
-                value="+998 (71) 2250015"
-                disabled
-              />
-              <a
-                href="tel:+9989712250015"
-                class="all px-6 py-[2px] rounded-l-2xl text-primary font-semibold cursor-pointer bg-whiteBlue"
-              >
-                {{ t("footer.call") }}
-              </a>
-            </div>
-          </form>
-        </div>
-
-        <div class="text-[#F4F6F9] text-lg font-light">2023. All rights reserved.</div>
+        <!-- <div class="SearchForm block xs:hidden"></div> -->
+        <div class="text-[#F4F6F9] text-lg font-light">Shohrux Soatov 506</div>
       </div>
     </div>
   </footer>
