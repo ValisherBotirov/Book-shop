295517ecd0539102a77f7c20503881590123bd98
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-07-04 15:18:36 +0500

    add banner

--- src/components/shop/SwiperBigComp.vue
+++ src/components/shop/SwiperBigComp.vue
@@ -13,12 +13,12 @@
     >
       <swiper-slide
         class="rounded-xl overflow-hidden"
-        v-if="store.banners.length"
-        v-for="val in store.banners"
+
+        v-for="val in fakeBanner"
         :key="val.id"
       >
         <RouterLink class="block aspect-10/3" :to="`/shop/${val.productId}`">
-          <img class="w-full h-full object-cover object-center" :src="val.imageUrl" :alt="val.title" />
+          <img class="w-full h-full object-cover object-center" src="https://img.freepik.com/premium-photo/books-stacked-library-shelf-bokeh-lights-generative-ai_255669-922.jpg?size=626&ext=jpg" :alt="val.title" />
         </RouterLink>
       </swiper-slide>
     </swiper>
@@ -33,9 +33,30 @@ import { RouterLink } from "vue-router";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Navigation, Autoplay } from "swiper";
-import { useBannerStore } from "../../store/banner";
+import { useBannerStore } from "@/store/banner.js";
 const store = useBannerStore();
 store.getAllBanners();
+
+const fakeBanner = [
+    {
+        id:1,
+        productId:1,
+        title:"Banner"
+
+    },
+    {
+        id:2,
+        productId:2,
+        title:"Banner"
+
+    },
+    {
+        id:3,
+        productId:3,
+        title:"Banner"
+
+    },
+]
 </script>
 
 <style scoped>
