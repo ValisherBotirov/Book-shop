ab95b091cf90d3fa9af1333a34b014a41f346c5a
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-07-05 17:43:13 +0500

    fix code

--- src/view/ShopSingleView.vue
+++ src/view/ShopSingleView.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto">
       <RouteLink class="mb-3 text-base" :route="routeLink" /> 
 
-      <div class="grid grid-cols-12 gap-8 my-8">
+      <div class="grid grid-cols-12 gap-8 my-8 mt-[60px] md:mt-8">
         <div class="swiperImg col-span-12 md:col-span-6 relative">
           <img class="rounded-xl w-full h-auto" :src="image" alt="product image" />
             <div class="absolute top-4 right-4">
