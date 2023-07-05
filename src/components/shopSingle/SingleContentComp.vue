d7a00b073d5a0a71ef4c3dae2612513a2652253b
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-07-05 17:25:27 +0500

    fix: shop single vue

--- src/components/shopSingle/SingleContentComp.vue
+++ src/components/shopSingle/SingleContentComp.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
       <h2 class="text-3xl mb-2 font-bold text-primaryBlue">
-          product name
+          Mahsulot nomi
       </h2>
-    <div class="grid grid-cols-5">
-      <div class="col-span-2 md:col-span-3 justify-between">
-        <p class="text-[#4F87D3CC] text-xl">Количество</p>
+      <div class="flex items-center gap-6 mt-2">
+        <p class="text-[#4F87D3CC] text-xl">Quantity:</p>
         <InputCount v-model="count" />
       </div>
-    </div>
-    <p class="text-[#4F87D3CC] text-xl mb-3">Характеристики</p>
+      <div class="flex items-center gap-6 mt-4">
+          <p class="text-[#4F87D3CC] text-xl ">Narxi:</p>
+          <p class="text-2xl font-bold text-[#4F87D3CC]">70 000 SUM</p>
+      </div>
+      <div class="flex items-center gap-6 mt-4">
+          <p class="text-[#4F87D3CC] text-xl ">Kategoriya:</p>
+          <p class="text-xl  text-[#4F87D3CC]">Biznes</p>
+      </div>
+      <div class="flex items-center gap-6 mt-4">
+          <p class="text-[#4F87D3CC] text-xl ">Nashriyoti:</p>
+          <p class="text-xl  text-[#4F87D3CC]">Asaxiy</p>
+      </div>
+      <div class="flex mt-6 items-center gap-6">
+          <SaveBasket class="w-full " >
+              Savatga qo'shish
+          </SaveBasket>
+          <SaveBasket class="w-full " :icon="false" custom-class="bg-[#79a206] !text-white border-none !hover:!bg-[#3E444A]">
+              Joyida sotib olish
+          </SaveBasket>
+      </div>
+
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import InputCount from "../../components/shop/InputCount.vue";
+import SaveBasket from "@/components/buttons/SaveBasket.vue";
 const count = ref(1);
 
 </script>
