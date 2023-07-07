9f7c49bf0329b21133e9312be5b3d20e8ef36078
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-07-07 17:10:07 +0500

    add basket style

--- src/components/card/BasketCard.vue
+++ src/components/card/BasketCard.vue
@@ -1,36 +1,31 @@
 <template>
   <div
-    class="card bg-white rounded-xl grid grid-cols-3 max-h-48 overflow-hidden"
+    class=" flex gap-3 max-h-48 overflow-hidden"
   >
-    <div class="img h-full col-span-1">
-      <router-link :to="`/shop/${item?.id}`">
+    <div class="flex-shrink-0 border border-[#e1e1e1] transition duration-300 hover:border-[#010101] p-1">
+      <router-link :to="`/shop/${item?.id}`" class=" hover:border cursor-pointer">
         <img
-          class="object-cover h-full w-full object-center"
+          class="object-cover w-[90px] h-[90px] object-center"
           :src="item?.image_url ? item.image_url : 'https://islom.uz/img/section/section_1503922927.jpg'"
           alt="cardImg"
         />
       </router-link>
     </div>
-    <div class="img rounded-xl bg-[#F4F6F9] flex flex-col gap-3 px-3 py-2 relative col-span-2">
+    <div class="relative ">
       <div
-        class="delete absolute top-2 right-3 cursor-pointer"
+        class="delete absolute top-0 right-0 cursor-pointer"
         @click="deleteProduct(item.id)"
       >
-        <i class="fa-solid fa-xmark text-lg hover:text-[red]"></i>
+        <i class="fa-solid fa-xmark text-base hover:text-[red]"></i>
       </div>
       <router-link
         :to="`/shop/${item?.id}`"
-        class="transition duration-100 hover:text-primaryBlue line-clamp-2"
+        class="transition duration-100 hover:text-primaryBlue line-clamp-2 pr-7"
       >
-        {{ item?.name }} Shayx Muhammad Sodiq Muhammad Yusuf - Kifoya
+        {{ item?.name }} Shayx Muhammad Sodiq   - Kifoya
       </router-link>
 
-      <div class="flex justify-between items-center flex-wrap">
-        <p class="font-semibold">
-          {{ numberWithSpaces(item?.product_detail?.price) }} сум
-        </p>
-        <InputCount v-model="item.count" />
-      </div>
+        <p class="mt-2 text-sm">{{item?.count}} x <span class="font-medium">{{ numberWithSpaces(5000) }} UZS</span> </p>
     </div>
   </div>
 </template>
