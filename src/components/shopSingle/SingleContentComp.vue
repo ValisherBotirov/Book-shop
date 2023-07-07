6fcc9ba9dc3bef8fbf75aa2269f817abcfea1403
Ikromov-fs <nodirikromov10@gmail.com>
2023-07-07 18:53:21 +0500

    single pahes full

--- src/components/shopSingle/SingleContentComp.vue
+++ src/components/shopSingle/SingleContentComp.vue
@@ -1,31 +1,67 @@
 <template>
-  <div class="flex">
-    <div class="w-[50%]">
+  <div class="md:flex block sx:py-5 lg:py-0">
+    <div class="md:w-[50%] w-[100%]">
       <ul>
-        <li class="flex justify-between px-10 text-start items-center">
-          <span>Til</span
-          ><span
-            class="iconAn w-[100%] border-dashed border-[1px] border-[#333] opacity-[0.5]"
-          ></span
-          ><span>O'zbekcha</span>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Til</span><span>O'zbekcha</span>
         </li>
-        <li class="flex justify-between px-10 text-start items-center">
-        <span>Yozuv</span><span class="iconAn2 w-[100%] border-dashed border-[1px] border-[#333] opacity-[0.5]"></span><span>Kirilcha</span>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Yozuv</span><span>Krilcha</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Sana</span><span>10.09.2023</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Nashriyot</span><span>Hilol nashr</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Sana</span><span>10.09.2023</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Sahifalar soni</span><span>100</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Sana</span><span>10.09.2023</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Size</span><span>A4</span>
+        </li>
+        <li class="flex justify-between md:px-10 text-start items-center">
+          <span>Sotuvda</span><span>Bor</span>
+        </li>
+      </ul>
+      <ul class="md:px-10 lg:px-[26px] xl:px-10 pt-12 flex justify-between gap-3">
+        <li
+          class="translation flex items-center gap-3 cursor-pointer justify-center bg-[#002E69] rounded-md w-[50%] py-2 text-white hover:bg-transparent hover:text-black hover:border-black border-[1px] hover:border-solid"
+        >
+          <i class="iconAn fa-solid fa-headphones"></i>Kitob audiosi
+        </li>
+        <li
+          class="translation flex items-center gap-3 cursor-pointer justify-center bg-[#002E69] rounded-md w-[50%] py-2 text-white hover:bg-transparent hover:text-black hover:border-black border-[1px] hover:border-solid"
+        >
+          <i class="iconAn fa-solid fa-book-open"></i> Kitobni o'qish
         </li>
       </ul>
     </div>
-    <div
-      class="border-[#9d9a9a] border-solid border-[2px] p-5 rounded-xl w-[50%]"
-    >
-      <div class="flex items-end gap-3">
+    <div class="border-[#9d9a9a] border-solid border-[2px] p-5 sx:mt-3 md:mt-0 rounded-xl sx:w-[100%] md:w-[50%]">
+      <div class="flex items-end gap-3 relative">
         <h1 class="font-medium text-2xl">40 000 so'm</h1>
         <del class="font-medium text-xl opacity-[0.5]">52 000 so'm</del>
+        <strong
+          class="iconAn sx:text-[17px] md:text-[20px] absolute right-[-75px] top-10 rotate-45 bg-[red] text-white px-3 py-1 rounded-md"
+          ><i class="fa-solid fa-bookmark mx-2"></i>Supper chegirma
+          <span>32</span><i class="mx-1 fa-solid fa-percent"></i
+        ></strong>
+      </div>
+      <div class="flex items-center gap-3 pt-4 font-medium">
+        <p class="text-2xl">Umumiy so'm:</p>
+        <h2 class="text-2xl">40 000 so'm</h2>
       </div>
       <div class="flex items-center gap-6 mt-2 my-2">
         <p class="text-[#333] text-2xl">Miqdori:</p>
         <InputCount v-model="count" />
       </div>
-      <div class="flex flex-col gap-3">
+      <div class="flex flex-col gap-3 pt-5">
         <SaveBasket class="w-full"> Savatga qo'shish </SaveBasket>
         <SaveBasket
           class="w-full"
@@ -46,6 +82,9 @@ import SaveBasket from "@/components/buttons/SaveBasket.vue";
 const count = ref(1);
 </script>
 <style scoped>
+.translation {
+  transition: all 0.2s linear;
+}
 .iconAn {
   animation: 3s linear 1s infinite alternate example;
 }
@@ -57,32 +96,16 @@ const count = ref(1);
   0% {
   }
   25% {
-    opacity: 0;
+    opacity: 0.2;
   }
   50% {
     opacity: 1;
   }
   75% {
-    opacity: 0;
+    opacity: 0.2;
   }
   100% {
     opacity: 1;
-  }
-}
-@keyframes example2 {
-  0% {
-  }
-  25% {
-    opacity: 1;
-  }
-  50% {
-    opacity: 0;
-  }
-  75% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0;
   }
 }
 </style>
