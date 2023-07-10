a964a131c6bdc682e1cd26eca10b1918838e143c
Ikromov-fs <nodirikromov10@gmail.com>
2023-07-10 19:16:12 +0500

    category

--- src/components/shopCategory/CategoryFilter.vue
+++ src/components/shopCategory/CategoryFilter.vue
@@ -1,42 +1,36 @@
 <template>
-  <div>
+  <div class="sticky top-[200px]">
     <!-- Decktop category -->
-    <div class="md:flex hidden justify-center mt-5 py-5 md:w-[90%] lg:w-[80%] bg-[#fff] rounded-xl shadow-card">
-      <ul class="flex flex-col gap-3 text-left">
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Biznes kitoblar</a>
+    <div
+      class="flex flex-col items-center py-5 md:w-[90%] lg:w-[80%] bg-[#fff] rounded-xl shadow-card"
+    >
+      <div class="flex flex-col">
+        <el-checkbox v-model="checked1" label="Option 1" size="large" />
+        <el-checkbox v-model="checked2" label="Option 2" size="large" />
+      </div>
+    </div>
+    <!-- Books type checkbox -->
+    <div
+      class="md:flex hidden justify-center mt-5 py-5 md:w-[90%] lg:w-[80%] bg-[#fff] rounded-xl shadow-card"
+    >
+      <ul class="grid grid-cols-1 gap-3 w-[100%] md:text-[16px] lg:text-[19px]">
+        <li class="cursor-pointer">
+          <label class="w-[100%] px-2 flex gap-3 pl-[20%]">
+            <input type="checkbox" class="cursor-pointer" />
+            <span class="cursor-pointer hover:text-blue-500">Qog'oz</span>
+          </label>
         </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Badiiy kitoblar</a>
+        <li class="cursor-pointer">
+          <label class="w-[100%] px-2 flex gap-3 pl-[20%]">
+            <input type="checkbox" class="cursor-pointer" />
+            <span class="cursor-pointer hover:text-blue-500">Audio</span>
+          </label>
         </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Darslik kitoblar</a>
-        </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Adabiyot kitoblar</a>
-        </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Romon kitoblar</a>
-        </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">She'r</a> kitoblar
-        </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Slavar kitoblar</a>
-        </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Slavar kitoblar</a>
-        </li>
-        <hr />
-        <li class="cursor-pointer hover:text-blue-700">
-          <a href="#">Slavar kitoblar</a>
+        <li class="cursor-pointer">
+          <label class="w-[100%] px-2 flex gap-3 pl-[20%]">
+            <input type="checkbox" class="cursor-pointer" />
+            <span class="cursor-pointer hover:text-blue-500">PDF</span>
+          </label>
         </li>
       </ul>
     </div>
@@ -57,6 +51,9 @@ const data = ref({
   color: "",
   typeMoney: "",
 });
+
+// checkbox element puls
+const checkList = ref(["selected and disabled", "Option A"]);
 
 async function rangeInp(rangeMinMaxArr) {
   data.value.range = rangeMinMaxArr;
