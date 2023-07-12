6b266b1a95451a6bdc1a3230b55773c92a61c627
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-07-12 12:45:30 +0500

    change axios base url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import i18n from "./i18n";
 
-// axios.defaults.baseURL = "https://plankton-app-vomkh.ondigitalocean.app/api/v1/";
-// axios.defaults.baseURL = "http://167.71.62.65:8000/api/v1";
-axios.defaults.baseURL = "https://seal-app-xlr8l.ondigitalocean.app/api/v1";
+axios.defaults.baseURL =
+  "https://globalbooks-production-74d9.up.railway.app/api/v1";
 axios.interceptors.request.use(
   (config) => {
     config.headers["Accept-Language"] = i18n.global.locale.value;
